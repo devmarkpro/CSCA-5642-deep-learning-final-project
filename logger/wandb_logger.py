7d87ed097da43a684ec2319c8917013017651c92
Mark Karamyar <mark.karamyar@gmail.com>
2025-10-01 05:41:23 +0200

    add logger to wandb

--- logger/wandb_logger.py
+++ logger/wandb_logger.py
@@ -1,9 +1,11 @@
 from config import AppConfig
+from logger.app_logger import get_logger
 
 
 class WandBLogger:
     def __init__(self, config: AppConfig):
         self.config = config
+        self.logger = get_logger(config.app_name)
 
     def log_metric(self, metric_name: str, metric_value: float, step: int):
         pass
